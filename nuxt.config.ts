1c95a3ad9501d3cfdb9d5d1d1de6d12d80101eef
Prelac F <prelacfilip@gmail.com>
2025-09-15 17:57:21 +0200

    Added the possibility to use .env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,25 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    emailPass: process.env.NUXT_EMAIL_PASS, 
+    public: {
+      emailUser: process.env.NUXT_PUBLIC_EMAIL_USER,  // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+    }
+  },
+
   modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image", "nuxt-mail"],
 
   mail: {
     message: {
-      to: '<EMAIL HERE>',
+      to: process.env.NUXT_PUBLIC_EMAIL_USER,
     },
     smtp: {
       host: "smtp.gmail.com",
       port: 587,
       auth: {
-          user: '<EMAIL HERE>',
-          pass: '<PASSWORD HERE>',
+          user: process.env.NUXT_PUBLIC_EMAIL_USER,
+          pass: process.env.NUXT_EMAIL_PASS,
         },
     },
   },
